72acda91fab64aff280006092530ce70fa6bef4a
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2024-05-22 13:51:27 +0330

    [feat] imp layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LayoutComponent } from './layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
