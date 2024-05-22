f67f1cfb4e3366147f2834abdcf5fc2263d072a1
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2024-05-22 14:00:11 +0330

    [feat] imp layout

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-layout',
+  selector: 'grid',
   standalone: true,
   imports: [],
   templateUrl: './layout.component.html',
