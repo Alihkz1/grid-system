6fb9651b3f032595478c2065f3d0d9e121d6549c
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2024-05-22 14:20:09 +0330

    [init] music player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LayoutComponent } from './layout/layout.component';
+import { MusicPlayerComponent } from './music-player/music-player.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LayoutComponent],
+  imports: [LayoutComponent, MusicPlayerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/music-player/music-player.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'music-player',
+  standalone: true,
+  imports: [],
+  templateUrl: './music-player.component.html',
+  styleUrl: './music-player.component.scss'
+})
+export class MusicPlayerComponent {
+
+}
