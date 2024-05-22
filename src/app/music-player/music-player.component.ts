0249321d28dc180fdc33f3a5ab0ddbd40735cece
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2024-05-22 15:15:55 +0330

    [wip] music-player

--- src/app/music-player/music-player.component.ts
+++ src/app/music-player/music-player.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component } from '@angular/core';
 
 @Component({
   selector: 'music-player',
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './music-player.component.html',
   styleUrl: './music-player.component.scss'
 })
-export class MusicPlayerComponent {
+export class MusicPlayerComponent implements AfterContentInit {
+  isPlaying = false;
+  audioPlayer: any;
+  duration: number;
+  passed: number;
+
+  ngAfterContentInit(): void {
+    this.audioPlayer = document.getElementById('audio');
+  }
+
+  onPlay() {
+    this.audioPlayer.play();
+    this.isPlaying = true;
+    this.duration = this.audioPlayer.duration;
+  }
+
+  onStop() {
+    this.audioPlayer.pause();
+    this.isPlaying = false;
+  }
 
 }
