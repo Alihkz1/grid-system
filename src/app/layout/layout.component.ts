551f582a7bbd169a0a5c07a5bc1c78891bc1c730
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2024-05-23 10:03:59 +0330

    [wip] resize boxes

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'grid',
@@ -8,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+  startResizeX: number;
+  isResizing: boolean;
+  resizeBoxId: number;
 
+  @HostListener('mousedown', ['$event'])
+  mouseDown(event: any) {
+    if (event.target.classList.contains('resize')) {
+      this.isResizing = true;
+      this.startResizeX = event.clientX;
+      this.resizeBoxId = +event.srcElement.id;
+    };
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  mouseMove(event: any) {
+    if (this.isResizing) {
+      const deltaX = event.clientX - this.startResizeX;
+      const box: any = document.getElementsByClassName('box')[this.resizeBoxId];
+      box.style['min-width'] = `${box.clientWidth + deltaX / 20}px`;
+      box.style['max-width'] = `${box.clientWidth + deltaX / 20}px`;
+    }
+  }
+
+  @HostListener('document:mouseup', ['$event'])
+  mouseUp() {
+    if (this.isResizing === true) this.isResizing = false;
+  }
 }
